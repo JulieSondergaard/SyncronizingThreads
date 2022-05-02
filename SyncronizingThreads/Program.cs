951f038144ab9e59d23ddd617c2f974925f4ba51
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2022-05-02 11:33:48 +0200

    Add project files.

---
+++ SyncronizingThreads/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace SyncronizingThreads
+{
+    class Program
+    {
+
+        static int count;
+        static object _lock = new object();
+
+
+        static void Main(string[] args)
+        {
+            Thread add = new Thread(Add);
+            Thread subtract = new Thread(Subtract);
+
+            add.Start();
+            subtract.Start();
+                        
+        }
+
+        static void Add()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+
+                count += 2;
+                Console.WriteLine("[Adding] Count:" + count);
+                Thread.Sleep(1000);
+
+                Monitor.Exit(_lock);
+            }
+        }
+
+        static void Subtract()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+
+                count--;
+                Console.WriteLine("[Subtracting] Count: " + count);
+                Thread.Sleep(1000);
+
+                Monitor.Exit(_lock);
+            }
+        }
+    }
+}
---
+++ SyncronizingThreads2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace SyncronizingThreads2
+{
+    class Program
+    {
+
+        static int count;
+
+        static object _lock = new object();
+
+
+        static void Main(string[] args)
+        {
+            Thread stars = new Thread(Print);
+            Thread hashtags = new Thread(Print);
+
+            stars.Start("*");
+            hashtags.Start("#");
+
+        }
+
+        static void Print(object symbol)
+        {
+            while (true)
+            {
+                lock (_lock)
+                {
+
+
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write(symbol.ToString());
+
+                        if (i == 59)
+                        {
+                            count += 60;
+                        }  
+                    }
+                    Console.Write($" {count}\n");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        
+    }
+}
